07044b54698d44ca4e600f4b5ff839fa52ed5972
AlissaStorm <Alissaalidib@hotmail.com>
2025-05-02 23:10:59 +0200

    Started on mappers to the entites

--- src/main/java/app/exceptions/tempexcpetion.java
+++
@@ -1,7 +0,0 @@
-package app.exceptions;
-
-public class tempexcpetion extends RuntimeException {
-    public tempexcpetion(String message) {
-        super(message);
-    }
-}
--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -1,4 +1,25 @@
 package app.persistence;
 
+import app.entities.BOMItem;
+import app.entities.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderMapper {
+
+    public static List<Order> getAllOrders(ConnectionPool connectionPool) { //TODO:
+
+        List<Order> orders = new ArrayList<Order>();
+        String query = "SELECT * FROM orders";
+        return orders;
+    }
+
+    public static List<BOMItem> getBOMItemByOrderId(ConnectionPool connectionPool, int orderId) {
+
+        String query = "SELECT * FROM bill_of_materials_view WHERE order_id = ?";
+
+    }
+
+
 }
