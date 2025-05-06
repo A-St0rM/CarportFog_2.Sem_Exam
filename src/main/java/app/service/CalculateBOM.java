2a6cef07aeb0ee0f8603d3d6bba932204f9d76ad
Jonathan0912000 <Jonathan0912@gmail.com>
2025-05-06 18:41:15 +0200

    Tilretning fra material til product

--- src/main/java/app/service/CalculateBOM.java
+++ src/main/java/app/service/CalculateBOM.java
@@ -5,7 +5,7 @@ import app.entities.ProductVariant;
 import app.entities.Order;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
-import app.persistence.MaterialMapper;
+import app.persistence.ProductMapper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,9 +41,9 @@ public class CalculateBOM {
         int quantity = calculatePolesQuantity();
 
         //Finde længde på stolperne - dvs variant
-        List<ProductVariant> materialVariants = MaterialMapper.getVariantsByProductIdAndMinLength(0, POLES, connectionPool);
-        ProductVariant materialVariant = materialVariants.get(0);
-        BOM bom = new BOM(0, quantity, "Stolper nedgraves 90cm i jorden", order, materialVariant);
+        List<ProductVariant> productVariants = ProductMapper.getVariantsByProductIdAndMinLength(0, POLES, connectionPool);
+        ProductVariant productVariant = productVariants.get(0);
+        BOM bom = new BOM(0, quantity, "Stolper nedgraves 90cm i jorden", order, productVariant);
 
         bomList.add(bom);
     }
