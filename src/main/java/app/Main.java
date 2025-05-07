ce01939425e52ff13274474747c22be0df4403a6
AlissaStorm <Alissaalidib@hotmail.com>
2025-05-07 08:17:00 +0200

    changed connection string

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -23,8 +23,8 @@ import com.sendgrid.helpers.mail.objects.Personalization;
 public class Main {
 
     private static final String USER = "postgres";
-    private static final String PASSWORD = "Ahlam1982";
-    private static final String URL = "jdbc:postgresql://157.230.98.129:5432/%s?currentSchema=public";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
     private static final String DB = "CarportFog";
 
     public static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
