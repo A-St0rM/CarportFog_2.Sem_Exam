ab68cfa68286a833249648b5a7d07e7dc7d9ffa5
AlissaStorm <Alissaalidib@hotmail.com>
2025-05-14 10:38:48 +0200

    update test with env variables

--- src/test/java/app/service/CalculateBOMTest.java
+++ src/test/java/app/service/CalculateBOMTest.java
@@ -20,8 +20,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculateBOMTest {
 
     private static final String USER = "postgres";
-    private static final String PASSWORD = "Ahlam1982";
-    private static final String URL = "jdbc:postgresql://157.230.98.129:5432/%s?currentSchema=public";
+    private static final String PASSWORD = System.getProperty("password");
+    private static final String URL = "jdbc:postgresql://" + System.getenv("ip") + ":5432/%s?currentSchema=public";
     private static final String DB = "CarportFog";
 
     public static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
